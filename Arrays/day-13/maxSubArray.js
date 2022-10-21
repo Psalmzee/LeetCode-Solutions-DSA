632f2ea9993f4b6b75ed3474975261ec0e99e5c6
Samson Okeji <engrsamsonokeji@gmail.com>
2022-10-21 22:37:38 +0100

    updated maxSubArray

--- Arrays/day-13/maxSubArray.js
+++ Arrays/day-13/maxSubArray.js
@@ -4,11 +4,15 @@
  */
 
  var maxSubArray = function(nums) {
-  let sum = nums[0];
-let maxSum = nums[0];
-for(let i=1;i<nums.length;i++){
-  sum = Math.max(sum+nums[i],nums[i]);
-  maxSum = Math.max(maxSum,sum);
-}
-return maxSum;
+  let sum = 0;
+  let max = nums[0]
+  
+  for(let i = 0; i <  nums.length; i++) {
+      sum += nums[i]
+      
+      if(sum > max) max = sum;
+      if(sum < 0) sum = 0
+  }
+  
+  return max
 };
